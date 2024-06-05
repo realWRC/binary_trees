df9d57115b88739140c3d6f51970f41f7a3d5865
Wongani <wonganichulu@gmail.com>
2024-06-05 16:27:53 +0200

    Added 9-binary_tree_height.c fix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,24 +9,24 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t height_r = 0;
 	size_t height_l = 0;
-	binary_tree_t *tmp = tree->right;
 
-	if (tree == NULL || tmp == NULL)
+	if (tree == NULL)
 		return (0);
-	while (tmp != NULL)
+	
+	if (tree->left)
 	{
-		tmp = tmp->right;
-		height_r++;
+		height_l = 1 + binary_tree_height(tree->left);
 	}
+	else
+		height_l = 0;
 
-	tmp = tree->left;
-	if (tmp == NULL)
-		return (height_r);
-	while (tmp != NULL)
+	if (tree->right)
 	{
-		tmp = tmp->left;
-		height_l++;
+		height_r = 1 + binary_tree_height(tree->right);
 	}
+	else
+		height_r = 0;
+
 	if (height_l > height_r)
 		return (height_l);
 	else
