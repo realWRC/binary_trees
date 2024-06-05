b72ecb0d0fdd5ef05be9cf7026058f84f2c3be85
Wongani <wonganichulu@gmail.com>
2024-06-05 05:15:57 +0200

    Added binary_trees.h and binary_tree_print.c

---
+++ binary_tree_print.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "binary_trees.h"
+
+/* Original code from http://stackoverflow.com/a/13755911/5184480 */
+
+/**
+ * print_t - Stores recursively each level in an array of strings.
+ *
+ * @tree: Pointer to the node to print.
+ * @offset: Offset to print.
+ * @depth: Depth of the node.
+ * @s: Buffer.
+ *
+ * Return: Length of printed tree after process.
+ */
+static int print_t(const binary_tree_t *tree, int offset, int depth, char **s)
+{
+}
